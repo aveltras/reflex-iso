e46d966a58be29eef9f6c7833644ecbf3c3142c7
aveltras <romain.viallard@outlook.fr>
2019-05-18 13:00:58 +0200

    Second try

--- src/common/Common.hs
+++ src/common/Common.hs
@@ -32,6 +32,9 @@ import Control.Monad.Fix (MonadFix)
 import GHCJS.DOM.Types (MonadJSM)
 import Reflex.Dom hiding (Error, HNil, Value)
 import Data.Text (Text, pack)
+import qualified Data.ByteString.Lazy as LBS
+import qualified Data.ByteString as BS
+import Data.Map.Strict (toList)
 
 main :: String
 main = "common"
@@ -55,12 +58,14 @@ withLocalDataSource h w = mdo
   eResponse <- performEvent $ liftIO . (traverseRequesterData h) <$> eRequest
   return val
 
-withWebSocketDataSource ::
+withWebSocketDataSource :: forall t m req x a.
   ( HasJSContext m
   , MonadFix m
   , MonadJSM (Performable m)
-  -- , ToJSON req
-  -- , FromJSON req
+  , ToJSON (req x)
+  , FromJSON (req x)
+  , ToJSON x
+  , FromJSON x
   , MonadJSM m
   , MonadHold t m
   , PerformEvent t m 
@@ -71,24 +76,44 @@ withWebSocketDataSource ::
   -> Bool -- reconnect on close
   -> WithDataSource t req m a -- widget
   -> m a
-withWebSocketDataSource url eClose reconnect w = mdo
-  let
-    eSend :: Event t [Maybe Value]
-    eSend = (fmap . fmap) (\(_reqKey :=> req) -> Just $ toJSON req) (requesterDataToList <$> eRequest)
+withWebSocketDataSource url _eClose _reconnect w = mdo
   (val, eRequest) <- runRequesterT w eResponse
-  (_eRawRequest, eResponse) <- matchResponsesWithRequests decoder eRequest (decodeValue <$> _webSocket_recv ws)
-  ws <- jsonWebSocket url $ WebSocketConfig eSend eClose reconnect []
+  (eMapRawRequest, eResponse) <- matchResponsesWithRequests decodeRes eRequest (fmapMaybe decodeTag (_webSocket_recv ws))
+  ws <- jsonWebSocket url $ def & webSocketConfig_send .~ (toList <$> eMapRawRequest) :: m (RawWebSocket t (Maybe (Int, x)))
   return val
-  where
-    decodeValue :: (FromJSON r) => Maybe Value -> r
-    decodeValue Nothing = error "error"
-    decodeValue (Just v) = 
-      case fromJSON v of
-        Error s -> error s
-        Success a -> a
 
-    decoder :: forall req x. (FromJSON x) => (req x) -> (Maybe Value, Maybe Value -> Identity x)
-    decoder req = (Just $ toJSON req, decodeValue)
+  where
+
+    decodeTag :: Value -> Maybe (Int, Value)
+    decodeTag = undefined
+    -- decodeTag bs =
+    --   case decodeStrict bs of
+    --     Nothing         -> Nothing :: Maybe (Int, Value)
+    --     Just (tag, val) -> Just (tag, val)
+
+    decodeRes :: (forall b. (ToJSON (req b), ToJSON b) => req b -> (Value, Value -> Identity b))
+    decodeRes = undefined
+
+  -- let
+  --   eSend :: Event t [Maybe Value]
+  --   eSend = (fmap . fmap) (\(_reqKey :=> req) -> Just $ toJSON req) (requesterDataToList <$> eRequest)
+  -- (val, eRequest) <- runRequesterT w eResponse
+  -- (_eRawRequest, eResponse) <- matchResponsesWithRequests undefined eRequest (decodeRequesterRes <$> _webSocket_recv ws)
+  -- ws <- jsonWebSocket url $ WebSocketConfig eSend eClose reconnect []
+  -- return val
+
+    -- decodeRequesterRes :: Maybe Value -> (Int, Maybe Value)
+    -- decodeRequesterRes mValue = undefined
+
+  --   decodeValue :: (FromJSON (req x), FromJSON x) => Maybe Value -> Identity x
+  --   decodeValue Nothing = error "error"
+  --   decodeValue (Just v) = 
+  --     case fromJSON v of
+  --       Error s -> error s
+  --       Success a -> a
+
+  --   decoder :: forall req x. (FromJSON x) => (req x) -> (Maybe Value, Maybe Value -> Identity x)
+  --   decoder req = (Just $ toJSON req, decodeValue)
 
 getResponse
   :: (HasDataSource t req m)
@@ -113,7 +138,7 @@ htmlW b = do
     el "body" $ do
       el "div" $ text "body"
       eResp <- getResponse ((RequestG1) <$ ePb)
-      _ <- widgetHold (text "Waiting for Response1") ((\b -> text ("Length is: " <> (pack . show $ b))) <$> eResp)
+      _ <- widgetHold (text "Waiting for Response1") ((\b -> text ("Length is: " <> (pack . show . not $ b))) <$> eResp)
       blank
 
 deriveJSONGADT ''RequestG
